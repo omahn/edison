6c4c283b1fb6a58e43c17ec9001dda29504acb87
PyNutter <delinuxbod@yahoo.co.uk>
2010-10-06 22:03:08 +0100

    Refactored the cmdb models to be more friendly

--- src/edison/cmdb/models.py
+++ src/edison/cmdb/models.py
@@ -5,66 +5,66 @@ from django.contrib.auth.models import User
 
 # First, Define our list of countries
 class Country(models.Model):
-    CountryName = models.CharField(max_length=255)
-    CountryCode = models.CharField(max_length=3)
+    Name = models.CharField(max_length=255)
+    Code = models.CharField(max_length=3)
 
     def __unicode__(self):
-        return self.CountryCode
+        return self.Code
     
     class Meta:
         #permissions = ()
         verbose_name = 'Country'
         verbose_name_plural = 'Countries'
-        ordering = ['CountryName']
+        ordering = ['Name']
     
     
 # Now define the counties/States that we can use
 class County(models.Model):
-    CountyName = models.CharField(max_length=128)
-    CountyCountry = models.ForeignKey('Country')
+    Name = models.CharField(max_length=128)
+    Country = models.ForeignKey('Country')
     
     def __unicode__(self):
-        return self.CountyName
+        return self.Name
     
     class Meta:
         #permissions = ()
         verbose_name = 'County'
         verbose_name_plural = 'Counties'
-        ordering = ['CountyName']
+        ordering = ['Name']
 
 # Where do people/things live?
 class Address(models.Model):
-    AddressLineOne = models.CharField(max_length=128)
-    AddressLineTwo = models.CharField(max_length=128,blank=True)
-    AddressLineThree = models.CharField(max_length=128,blank=True)
-    AddressPostcode = models.CharField(max_length=15)
-    AddressCounty = models.ForeignKey('County')
-    AddressCountry = models.ForeignKey('Country') 
+    LineOne = models.CharField(max_length=128)
+    LineTwo = models.CharField(max_length=128,blank=True)
+    LineThree = models.CharField(max_length=128,blank=True)
+    Postcode = models.CharField(max_length=15)
+    County = models.ForeignKey('County')
+    Country = models.ForeignKey('Country') 
 
     def __unicode__(self):
-        return u'%s, %s, %s' % (self.AddressLineOne, self.AddressCounty, self.AddressPostcode)
+        return u'%s, %s, %s' % (self.LineOne, self.County, self.Postcode)
     
     class Meta:
         #permissions = ()
         verbose_name = 'Address'
         verbose_name_plural = 'Addresses'
-        ordering = ['AddressLineOne']
+        ordering = ['LineOne']
 
 # What companies are there that we might want to talk to?
 class Company(models.Model):
-    CompanyName = models.CharField(max_length=255)
-    CompanyHeadOffice = models.ForeignKey('Address')
-    CompanySupportNumber = models.CharField(max_length=50)
-    CompanySupportEmail = models.EmailField()
+    Name = models.CharField(max_length=255)
+    HeadOffice = models.ForeignKey('Address')
+    SupportNumber = models.CharField(max_length=50)
+    SupportEmail = models.EmailField()
         
     def __unicode__(self):
-        return self.CompanyName
+        return self.Name
     
     class Meta:
         #permissions = ()
         verbose_name = 'Company'
         verbose_name_plural = 'Companies'
-        ordering = ['CompanyName']
+        ordering = ['Name']
     
 # A list of all our contacts both within and external to the company we work for
 class Contact(models.Model):
@@ -74,125 +74,125 @@ class Contact(models.Model):
                      ('Miss','Miss'),
                      ('Ms','Ms')
                      )
-    ContactTitle = models.CharField(max_length=6,choices=TITLE_CHOICES)
-    ContactFirstName = models.CharField(max_length=128)
-    ContactLastName = models.CharField(max_length=128)
-    ContactPrimaryPhone = models.CharField(max_length=50)
-    ContactEmailAddress = models.EmailField()
-    ContactCompany = models.ForeignKey('Company')
+    Title = models.CharField(max_length=6,choices=TITLE_CHOICES)
+    FirstName = models.CharField(max_length=128)
+    LastName = models.CharField(max_length=128)
+    PrimaryPhone = models.CharField(max_length=50)
+    EmailAddress = models.EmailField()
+    Company = models.ForeignKey('Company')
     
     def __unicode__(self):
-        return u'%s %s %s' % (self.ContactTitle, self.ContactFirstName, self.ContactLastName)
+        return u'%s %s %s' % (self.Title, self.FirstName, self.LastName)
     
     class Meta:
         #permissions = ()
         verbose_name = 'Contact'
         verbose_name_plural = 'Contacts'
-        ordering = ['ContactFirstName']
+        ordering = ['FirstName']
 
     
 # Our Datacentres
 class DataCentre(models.Model):
-    DataCentreName = models.CharField(max_length=255)
-    DataCentreShortCode = models.CharField(max_length=10)
-    DataCentreAddress = models.ForeignKey('Address')
-    DataCentrePrincipleContact = models.ForeignKey('Contact')
+    Name = models.CharField(max_length=255)
+    ShortCode = models.CharField(max_length=10)
+    Address = models.ForeignKey('Address')
+    PrincipleContact = models.ForeignKey('Contact')
 
     def __unicode__(self):
-        return self.DataCentreShortCode
+        return self.ShortCode
     
     class Meta:
         #permissions = ()
         verbose_name = 'Data Centre'
         verbose_name_plural = 'Data Centres'
-        ordering = ['DataCentreName']
+        ordering = ['Name']
 
 # The rooms in the datacentres
 class DataCentreRoom(models.Model):
-    DataCentreRoomName = models.CharField(max_length=25)
+    RoomName = models.CharField(max_length=25)
     DataCentre = models.ForeignKey('DataCentre')
     
     def __unicode__(self):
-        return u'%s in %s' % (self.DataCentreRoomName, self.DataCentre)
+        return u'%s in %s' % (self.RoomName, self.DataCentre)
     
     class Meta:
         #permissions = ()
         verbose_name = 'Data Centre Room'
         verbose_name_plural = 'Data Centre Rooms'
-        ordering = ['DataCentreRoomName']
+        ordering = ['RoomName']
     
 # The suites in the datacentres
 class DataCentreSuite(models.Model):
-    DataCentreSuiteName = models.CharField(max_length=128)
-    DataCentreRoom = models.ForeignKey('DataCentreRoom')
+    SuiteName = models.CharField(max_length=128)
+    Room = models.ForeignKey('DataCentreRoom')
     
     def __unicode__(self):
-        return u'%s -> %s' % (self.DataCentreSuiteName, self.DataCentreRoom)
+        return u'%s -> %s' % (self.SuiteName, self.Room)
     
     class Meta:
         #permissions = ()
         verbose_name = 'Data Centre Suite'
         verbose_name_plural = 'Data Centre Suites'
-        ordering = ['DataCentreSuiteName']
+        ordering = ['SuiteName']
 
 # The racks in the suites in the rooms in the datacentres....
 class DataCentreRack(models.Model):
-    DataCentreRackName = models.CharField(max_length=25)
-    DataCentreRoom = models.ForeignKey('DataCentreRoom',blank=True)
-    DataCentreSuite= models.ForeignKey('DataCentreSuite',blank=True)
+    RackName = models.CharField(max_length=25)
+    Room = models.ForeignKey('DataCentreRoom',blank=True)
+    Suite= models.ForeignKey('DataCentreSuite',blank=True)
     
     def __unicode__(self):
-        return u'%s -> %s (%s)' % (self.DataCentreRackName, self.DataCentreSuite, self.DataCentreRoom)
+        return u'%s -> %s (%s)' % (self.RackName, self.Suite, self.Room)
     
     class Meta:
         #permissions = ()
         verbose_name = 'Data Centre Rack'
         verbose_name_plural = 'Data Centre Racks'
-        ordering = ['DataCentreRackName']
+        ordering = ['RackName']
 
 # The different classes of configuration items
 class ConfigurationItemClass(models.Model):
-    ConfigurationItemClassName = models.CharField(max_length=100)
+    Name = models.CharField(max_length=100)
     
     def __unicode__(self):
-        return self.ConfigurationItemClassName
+        return self.Name
     
     class Meta:
         #permissions = ()
         verbose_name = 'Configuration Item Class'
         verbose_name_plural = 'Configuration Item Classes'
-        ordering = ['ConfigurationItemClassName']
+        ordering = ['Name']
 
 # The configuration items (servers/switches etc)
 class ConfigurationItem(models.Model):
-    ConfigurationItemHostname = models.CharField(max_length=255)
-    ConfigurationItemRack = models.ForeignKey('DataCentreRack')
-    ConfigurationItemAsset = models.CharField(max_length=128)
-    ConfigurationItemSupportTag = models.CharField(max_length=128)
-    ConfigurationItemClass = models.ForeignKey(ConfigurationItemClass)
-    ConfigurationItemOwner = models.ForeignKey(User)
+    Hostname = models.CharField(max_length=255)
+    Rack = models.ForeignKey('DataCentreRack')
+    Asset = models.CharField(max_length=128)
+    SupportTag = models.CharField(max_length=128)
+    Class = models.ForeignKey(ConfigurationItemClass)
+    Owner = models.ForeignKey(User)
     
     def __unicode__(self):
-        return self.ConfigurationItemHostname
+        return self.Hostname
     
     class Meta:
         #permissions = ()
         verbose_name = 'Configuration Item'
         verbose_name_plural = 'Configuration Items'
-        ordering = ['ConfigurationItemHostname']
+        ordering = ['Hostname']
         
 # The network interfaces that are assigned to configuration items
 class NetworkInterface(models.Model):
-    NetworkInterfaceName = models.CharField(max_length=5)
-    NetworkInterfaceMacAddress = models.CharField(max_length=30)
-    NetworkInterfaceIPAddress = models.IPAddressField()
+    Name = models.CharField(max_length=5)
+    MacAddress = models.CharField(max_length=30)
+    IPAddress = models.IPAddressField()
     ConfigurationItem = models.ForeignKey('ConfigurationItem')
     
     def __unicode__(self):
-        return self.NetworkInterfaceName    
+        return self.Name    
     
     class Meta:
         #permissions = ()
         verbose_name = 'Network Interface'
         verbose_name_plural = 'Network Interfaces'
-        ordering = ['NetworkInterfaceName']
+        ordering = ['Name']
