19a13d0f08cbfeb5297656e3836c663fa3d572dd
Prof Falken <theprofessor@threedrunkensysadsonthe.net>
2010-10-15 16:06:50 +0100

    updated API to include the (basic) output of puppet classes

--- src/edison/api/handlers.py
+++ src/edison/api/handlers.py
@@ -1,12 +1,28 @@
 from piston.handler import BaseHandler
+from edison.orchestra.models import *
 from edison.cmdb.models import *
 
 class CfgItemHandler(BaseHandler):
 	allowed_methods = ('GET',)
-	fields = ('Hostname',('Rack',('Suite',('Room',(),),),),'Asset','DataCentre')
-	model = ConfigurationItem,DataCentreRack,DataCentreSuite,DataCentreRoom
-
 
 	def read(self,request,hostname):
-		data = ConfigurationItem.objects.get(Hostname=hostname)
+		results = ConfigurationItem.objects.select_related().get(Hostname=hostname)
+		serverDetails = results
+		locationDetails = results.Rack
+		data = {'hostname' : serverDetails.Hostname, \
+		'dc_rack' : locationDetails.RackName, \
+		'dc_suite' : locationDetails.Suite.SuiteName, \
+		'dc_room': locationDetails.Room.RoomName, \
+		'datacentre' : locationDetails.Room.DataCentre.Name, \
+		'item_class': serverDetails.Class.Name}
+		return data
+
+class PuppetHandler(BaseHandler):
+	allowed_methods = ('GET')
+
+	def read(self,request,hostname):
+		results = OrchestraClass.objects.select_related('Name','Hostname').filter(AffectedItems__Hostname__icontains = hostname)
+		data = [] 
+		for result in results: 
+			data.append(result.Name)
 		return data
--- src/edison/api/urls.py
+++ src/edison/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
 
-from edison.api.handlers import CfgItemHandler
+from edison.api.handlers import *
 
 cfgitem_resource = Resource(handler=CfgItemHandler)
+puppet_resource = Resource(handler=PuppetHandler)
 
 urlpatterns = patterns('',
+    url(r'^puppet/(?P<hostname>[^/]+)/$', puppet_resource), 
     url(r'^hosts/(?P<hostname>[^/]+)/$', cfgitem_resource), 
 )
