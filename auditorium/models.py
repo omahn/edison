445f7df48e5cf7a7b23bbd653ba0fa1614f5281b
proffalken <theprofessor@threedrunkensysadsonthe.net>
2010-10-25 19:48:07 +0100

    added auditorium - the auditing tool - only has model defined at present

---
+++ auditorium/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from cmdb.models import ConfigurationItem
+# Create your models here.
+
+# log the packages that are updated every time the package manager runs
+#
+# Need to write a yum/apt-plugin to post to the API for this...
+class Package(models.Model):
+	Name = models.CharField(max_length=255)
+	Version = models.CharField(max_length=255)
+	Repository = models.CharField(max_length=255)
+	AffectedItem = models.ForeignKeyField(ConfigurationItem)
+
+	def __unicode__(self):
+		return u'%s - %s' % (self.Name,self.Version)
+
+	class Meta:
+		verbose_name = 'Software Package'
+		verbose_name_plural = 'Software Packages'
+		ordering = ['Name','Version']
+
+
