0aee673da5d309f209d6f37a1722b22a7668750d
proffalken <theprofessor@threedrunkensysadsonthe.net>
2010-11-01 20:33:59 +0000

    Copyright notice at the top of "manage.py" was throwing the #! exec - removed notice as not covered by Edison Project

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-# This file is part of the Edison Project.
-# Please refer to the LICENSE document that was supplied with this software for information on how it can be used.
 #!/usr/bin/python
 from django.core.management import execute_manager
 try:
